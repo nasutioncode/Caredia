6b673fb9a06e36446d4579a2560228d4c8ef0aa7
nasutioncode <nasutioncode@gmail.com>
2019-12-05 20:15:41 +0700

    nas coba 22222222

---
+++ app/src/main/java/com/nasution/cardiacare/Ds.java
@@ -0,0 +1,14 @@
+package com.nasution.cardiacare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ds extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ds);
+    }
+}
