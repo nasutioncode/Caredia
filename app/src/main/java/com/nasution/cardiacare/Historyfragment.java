0970d62c3d67ea583958a79a4612a402938c4af7
nasutioncode <nasutioncode@gmail.com>
2019-12-05 20:06:18 +0700

    nas coba

--- app/src/main/java/com/nasution/cardiacare/Historyfragment.java
+++ app/src/main/java/com/nasution/cardiacare/Historyfragment.java
@@ -7,8 +7,12 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class Historyfragment extends Fragment {
 
@@ -33,6 +37,49 @@ public class Historyfragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.historyfragment,container,false);
+
+
+        ArrayList<HistoryItem> exampleList = new ArrayList<>();
+        exampleList.add(new HistoryItem("tanggal", "12/12/12", "resiko"));
+        exampleList.add(new HistoryItem("tanggal", "12/12/12", "resiko"));
+        exampleList.add(new HistoryItem("tanggal", "12/12/12", "resiko"));
+
+//        //mRecyclerView = findViewById(R.id.recyclerview);
+//        mRecyclerView.setHasFixedSize(true);
+//        mLayoutManager = new LinearLayoutManager(this);
+//        mAdapter = new HistoryAdapter(exampleList);
+//
+//        mRecyclerView.setLayoutManager(mLayoutManager);
+//        mRecyclerView.setAdapter(mAdapter);
+
+
         return view;
     }
+
+
+    public class History extends AppCompatActivity {
+        private RecyclerView mRecyclerView;
+        private RecyclerView.Adapter mAdapter;
+        private RecyclerView.LayoutManager mLayoutManager;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.historyfragment);
+
+            ArrayList<HistoryItem> exampleList = new ArrayList<>();
+            exampleList.add(new HistoryItem("tanggal", "12/12/12", "resiko"));
+            exampleList.add(new HistoryItem("tanggal", "12/12/12", "resiko"));
+            exampleList.add(new HistoryItem("tanggal", "12/12/12", "resiko"));
+
+            mRecyclerView = findViewById(R.id.recyclerview);
+            mRecyclerView.setHasFixedSize(true);
+            mLayoutManager = new LinearLayoutManager(this);
+            mAdapter = new HistoryAdapter(exampleList);
+
+            mRecyclerView.setLayoutManager(mLayoutManager);
+            mRecyclerView.setAdapter(mAdapter);
+        }
+    }
+
 }
